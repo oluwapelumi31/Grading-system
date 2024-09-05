d91a457cbfd5e4585a901d175d77927538dd30b0
oluwapelumi31 <ayomiposipelumi31@gmail.com>
2024-09-05 15:06:47 +0100

    update

---
+++ JAVA SCRIPT CLASS(LEVEL 2)/assignmentoperators(output).js
@@ -0,0 +1,19 @@
+// // var myAge = 10;
+// // // myAge *= 2
+// // // myAge++
+// // // myAge++
+// // alert(myAge);
+// var myAge = 10;
+// // console.log(myAge);
+// document.getElementById("myinput").value
+function ageTeller () {
+//    document.getElementById("myinput").value
+   var userAge = myinput.value
+//    alert("your age is" +userAge )
+//    alert(`your age is ${userAge}`)
+// alert(Number(userAge)+2)
+// alert(parseInt(userAge)+2)
+// alert(parseFloat(userAge)-2)
+disp.innerText = "Your age is " + userAge
+}
+
---
+++ JAVA SCRIPT CLASS(LEVEL 2)/money converter.js
@@ -0,0 +1,35 @@
+function naira(){
+    var converter = prompt("Input your amount")
+    var con=900
+    sop=con*converter
+     if(con*converter)  {
+        alert("$"+sop)
+     }
+    }
+     function dollar(){
+        var converter = prompt("Amount in dollar")
+        var miracle=900
+        Rejoice=converter/miracle
+        if(converter/miracle){
+            alert(Rejoice)
+        }
+     }
+      function pounds(){
+        var converter = prompt("Amount in naira")
+        var Testimony=849
+        dog=Testimony*converter
+        if(Testimony*converter)
+        alert(""+dog)
+      }
+       function euro(){
+        var converter = prompt("Amount in naira")
+        var Fire=546
+        Bark=Fire*converter
+        if(Fire*converter)
+        alert("£ " +Bark)
+       }
+       
+         var mySong = new Audio("halleluyah.mp3")
+         function playSong(){
+         mySong.play()
+       }
